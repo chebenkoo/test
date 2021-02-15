da3d8bb928dd122cc0a4d183ceb60c2e31086f90
Leonid Ch <lionchebenko@gmail.com>
2021-02-15 11:06:31 +0200

    2d commit

--- sensor.c
+++ sensor.c
@@ -1,2 +1,11 @@
-include somwthing 
-:
+#include <stdio.h>
+
+
+char *ptr = NULL;
+
+int main(int argc, char** argv)
+{
+    printf("Git-Version: %s\n, %s\n:", VERSION, TAGCOMMIT);
+
+	return 0; 
+}
