814eea512e2ae74ef1efb4e2c6c034c6383b2383
Leonid Ch <lionchebenko@gmail.com>
2021-02-15 11:08:17 +0200

    add function2()

--- sensor.c
+++ sensor.c
@@ -3,9 +3,17 @@
 
 char *ptr = NULL;
 
+void fun2();
+
 int main(int argc, char** argv)
 {
     printf("Git-Version: %s\n, %s\n:", VERSION, TAGCOMMIT);
 
 	return 0; 
+}
+
+
+void fun2()
+{
+	printf("inside fucntion 2");
 }
